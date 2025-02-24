c8c5cb197807f6f283fd9577e492ce68b63ff784
0xFAROUK <hadjhacenefarouk@gmail.com>
2025-02-24 17:08:05 +0100

    Update Encryption.h

--- Encryption-repo/Encryption.h
+++ Encryption-repo/Encryption.h
@@ -1,1 +1,10 @@
-
+#ifndef ENCRYPTION_H_INCLUDED
+#define ENCRYPTION_H_INCLUDED
+#include<stdio.h>
+#include'Stringslibrary.h'
+#include <stdbool.h>
+void atbashCipher(char *text);
+void caesarCipher(char *text, int shift) ;
+void vigenereCipher(char *text, const char *key);
+void xorCipher(char *text, char key) ;
+#endif //  NUMBERSLIBRARY_H_INCLUDED
